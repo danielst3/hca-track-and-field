edcbc5f5f93c04d12951a8f79586082587ec4bb8
Tom Daniels <tom.daniels@hcakc.org>
2026-02-12 20:14:48 +0000

    File changes

--- src/pages/Athletes.jsx
+++ src/pages/Athletes.jsx
@@ -17,7 +17,6 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
 
 export default function Athletes() {
