b2e51eae1b53810b008db55ff0495b2dd9415763
Tom Daniels <tom.daniels@hcakc.org>
2026-02-12 22:41:37 +0000

    File changes

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -55,6 +55,9 @@ export default function Calendar() {
     const fetchUser = async () => {
       const currentUser = await base44.auth.me();
       setUser(currentUser);
+      if (currentUser?.default_events && currentUser.default_events.length > 0) {
+        setSelectedEvents(currentUser.default_events);
+      }
     };
     fetchUser();
   }, []);
